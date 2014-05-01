82304f99377efba2989d3d40debce60191d08b2b
Brian Arnold <brianarn@gmail.com>
2014-05-01 00:14:16 -0600

    Step 7: Add custom rendering

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ require([
   'esri/map',
   'esri/layers/FeatureLayer',
   'esri/InfoTemplate',
+  'esri/Color',
+  'esri/symbols/SimpleLineSymbol',
+  'esri/symbols/SimpleFillSymbol',
+  'esri/renderers/SimpleRenderer',
   'dojo/domReady!'
-], function(Map, FeatureLayer, InfoTemplate) {
+], function(Map, FeatureLayer, InfoTemplate, Color, SimpleLineSymbol, SimpleFillSymbol, SimpleRenderer) {
   // URL to our feature layer
   var url = 'http://gis-web.heritage.unm.edu/arcgis/rest/services/DataMgmt/MinimumConvexPolygon/MapServer/0';
 
@@ -30,6 +34,24 @@ require([
 
       // Let's add a definition expression
       featureLayer.setDefinitionExpression('Shape_Area < 2000000000 AND Shape_Area > 1000000000');
+
+      // Create our SimpleLineSymbol for shape borders
+      var outline = new SimpleLineSymbol(
+        SimpleLineSymbol.STYLE_SOLID, // Line style
+        new Color('black'), // Line color
+        2 // Line width
+      );
+
+      // Create our fill symbol
+      var fillSymbol = new SimpleFillSymbol(
+        SimpleFillSymbol.STYLE_SOLID, // Fill style
+        outline, // Outline symbol as defined above
+        new Color([0, 0, 255, 0.25]) // Fill color, Blue at 25% opacity
+      );
+
+      // Create a renderer for use with the feature layer, using our fill symbol
+      var renderer = new SimpleRenderer(fillSymbol);
+      featureLayer.setRenderer(renderer);
 
       map.addLayer(featureLayer);
     }
